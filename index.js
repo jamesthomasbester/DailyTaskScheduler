3d90efae165e8db3915ce2edd62fcafcb2512a7d
James Bester <46641259+jamesthomasbester@users.noreply.github.com>
2022-03-22 13:47:00 +1100

    core update via gui
    added in some javascript functions to show the current time and change the colour of the schedule based on the current time.
    added the schedule table as well

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("working")
+
+const hours = [7,8,9,10,11,12,13,14,15,16,17,18];
+var currentHour = parseInt(moment().startOf('day').fromNow().substring(0, 2));
+
+
+
+hours.forEach(element => {
+    if(element < currentHour){
+        $(`#${element}`).css("background-color", "tomato");
+    }else if(element = currentHour){
+        $(`#${element}`).css("background-color", "#77dd77");
+    }else if(element > currentHour){
+        $(`#${element}`).css("background-color", "grey");
+    }
+});
+
+window.timer = setInterval(() => {
+    $('#currentDay').text(moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"));
+})
